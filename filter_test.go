6f6495c0eeb9b161ab5dfe0120cc6b9cf06ae6bc
asuka <312856403@qq.com>
2022-02-15 09:39:36 +0800

    update
    Signed-off-by: asuka <312856403@qq.com>

--- filter_test.go
+++ filter_test.go
@@ -2,12 +2,201 @@ package exp
 
 import (
 	"encoding/json"
+	log "github.com/sirupsen/logrus"
 	"math/rand"
 	"os"
 	"sort"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
+
+var bf *BinaryFuse8
+
+var bfs []*BinaryFuse8
+
+func BenchmarkBinaryFuse8Multiple(b *testing.B) {
+	N := 40000000
+	numFilters := 8
+	setupMultiple(N, numFilters, false)
+	key := uint64(0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		for k := 0; k < numFilters; k++ {
+			bfs[k].Contains(key)
+		}
+		key++
+	}
+}
+
+func BenchmarkPopulateFuse8SingleParallel(b *testing.B) {
+	N := 160000
+	sortedGlobal := false
+	setupSingle(N, sortedGlobal)
+	key := uint64(0)
+
+	b.ResetTimer()
+	b.SetParallelism(2)
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			bf.Contains(key)
+			key++
+		}
+	})
+}
+
+func BenchmarkPopulateFuse8Single(b *testing.B) {
+	//log.Info(runtime.GOMAXPROCS(0))
+	N := 100000000
+	//numFilters := 1
+	sortedGlobal := true
+	//setupMultiple(N, numFilters, sortedGlobal)
+	setupSingle(N, sortedGlobal)
+	//key := uint64(0)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		//for k := 0; k < numFilters; k++ {
+		//	bfs[k].Contains(uint64(i))
+		//}
+		//bf.Contains(key)
+		//key++
+		bf.Contains(0)
+	}
+}
+
+func TestXXX(t *testing.T) {
+	N := 1600000
+	numFilters := 1
+	sortedGlobal := false
+	setupMultiple(N, numFilters, sortedGlobal)
+	key := rand.Uint64() % uint64(N)
+
+	st := time.Now()
+	for k := 0; k < numFilters; k++ {
+		bfs[k].Contains(key)
+	}
+	ed := time.Since(st)
+	log.Info(ed.Nanoseconds(), " ns")
+}
+
+func TestBinaryFuseFuse8Multiple(t *testing.T) {
+	N := 32000000
+	concurrently := false
+	numFilters := 20
+	sortedGlobal := false
+	batchSize := 1600000
+	queryData := fetchData(N, false)
+	//queryData = make([]uint64, 1000)
+	setupMultiple(N, numFilters, sortedGlobal)
+
+	//querySingleThread := func(key uint64) {
+	//	for k := 0; k < numFilters; k++ {
+	//		bfs[k].Contains(key)
+	//	}
+	//}
+
+	queryMultiThreads := func(key uint64) {
+		var wg sync.WaitGroup
+		wg.Add(numFilters)
+		for k := 0; k < numFilters; k++ {
+			go func(k int) {
+				bfs[k].Contains(key)
+				wg.Done()
+			}(k)
+		}
+		wg.Wait()
+	}
+
+	if concurrently {
+		start := time.Now()
+		for i := 0; i < len(queryData); i++ {
+			queryMultiThreads(queryData[i])
+		}
+		end := time.Since(start)
+		log.Info(float64(end.Nanoseconds()) / float64(len(queryData)), " ns/operation")
+	} else {
+		start := time.Now()
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			for i := 0; i < len(queryData) / batchSize; i++ {
+				//querySingleThread(queryData[i])
+				data := queryData[i * batchSize : (i + 1) * batchSize]
+				for k := 0; k < numFilters; k++ {
+					for j := 0; j < batchSize; j++ {
+						bfs[k].Contains(data[j])
+					}
+				}
+			}
+
+			//for k := 0; k < numFilters; k++ {
+			//	for i := 0; i < len(queryData); i++ {
+			//		bfs[k].Contains(queryData[i])
+			//	}
+			//}
+			wg.Done()
+		}()
+		wg.Wait()
+		end := time.Since(start)
+		log.Info(float64(end.Nanoseconds()) / float64(len(queryData)), " ns/operation")
+	}
+}
+
+func TestBinaryFuse8Single(t *testing.T) {
+	N := 5000000
+	numThreads := 1
+	setupSingle(N, true)
+	queryData := fetchData(N * 20, false)
+	//for i := 0; i < len(queryData); i++ {
+	//	queryData[i] += uint64(N)
+	//}
+
+	var wg sync.WaitGroup
+	reader := func(data []uint64) {
+		for i := 0; i < len(data); i++ {
+			bf.Contains(queryData[i])
+		}
+		wg.Done()
+	}
+
+	start := time.Now()
+	for k := 0; k < numThreads; k++ {
+		data := queryData[k * len(queryData) / numThreads: (k + 1) * len(queryData) / numThreads]
+		wg.Add(1)
+		go reader(data)
+	}
+	wg.Wait()
+	end := time.Since(start)
+
+	log.Info(float64(end.Nanoseconds()) / float64(len(queryData)), " ns/operation")
+}
+
+func setupSingle(N int, sorted bool) {
+	if bf != nil {
+		return
+	}
+	log.Info("setup filter begin ...... size: ", N)
+	bf, _ = PopulateBinaryFuse8(fetchData(N, sorted))
+	log.Info("setup filter end ...... size: ", N)
+}
+
+func setupMultiple(N int, numFilters int, sortedGlobal bool) {
+	if len(bfs) != 0 {
+		return
+	}
+	log.Info("setup filters begin ...... size: ", N, " numFilters: ", numFilters)
+	dataTotal := fetchData(N, sortedGlobal)
+	bfs = make([]*BinaryFuse8, 0)
+	for i := 0; i < numFilters; i++ {
+		dataLocal := dataTotal[i * N / numFilters : (i + 1) * N / numFilters]
+		bf, _ := PopulateBinaryFuse8(dataLocal)
+		bfs = append(bfs, bf)
+	}
+	log.Info("setup filters end ...... size: ", N, " numFilters: ", numFilters)
+}
 
 func fetchData(N int, sorted bool) []uint64 {
 	if sorted {
@@ -62,7 +251,7 @@ func genData(N int, sorted bool) []uint64 {
 }
 
 func TestDataGenerator(t *testing.T) {
-	sizes := []int{160000, 500000, 1600000, 5000000, 10000000, 50000000, 100000000}
+	sizes := []int{51200000/*160000, 500000, 1600000, 5000000, 10000000, 50000000, 100000000*/}
 	for _, size := range sizes {
 		data := genData(size, true)
 		filename := "/Users/asuka/filterexp/data/" + strconv.Itoa(size) + ".sorted"
---
+++ origin_test.go
@@ -0,0 +1,196 @@
+package exp
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+)
+
+const NUM_KEYS = 100000000
+const MID_NUM_KEYS = 11500
+const SMALL_NUM_KEYS = 100
+
+
+func TestBinaryFuse8Basic(t *testing.T) {
+	keys := make([]uint64, NUM_KEYS)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+	filter, _ := PopulateBinaryFuse8(keys)
+	for _, v := range keys {
+		assert.Equal(t, true, filter.Contains(v))
+	}
+	falsesize := 10000000
+	matches := 0
+	bpv := float64(len(filter.Fingerprints)) * 8.0 / float64(NUM_KEYS)
+	fmt.Println("Binary Fuse8 filter:")
+	fmt.Println("bits per entry ", bpv)
+	for i := 0; i < falsesize; i++ {
+		v := rand.Uint64()
+		if filter.Contains(v) {
+			matches++
+		}
+	}
+	fpp := float64(matches) * 100.0 / float64(falsesize)
+	fmt.Println("false positive rate ", fpp)
+	cut := 1000
+	if cut > NUM_KEYS {
+		cut = NUM_KEYS
+	}
+	keys = keys[:cut]
+	for trial := 0; trial < 10; trial++ {
+		rand.Seed(int64(trial))
+		for i := range keys {
+			keys[i] = rand.Uint64()
+		}
+		filter, _ = PopulateBinaryFuse8(keys)
+		for _, v := range keys {
+			assert.Equal(t, true, filter.Contains(v))
+		}
+
+	}
+}
+
+func TestBinaryFuse8Issue23(t *testing.T) {
+	for trials := 0; trials < 20; trials++ {
+		keys := make([]uint64, MID_NUM_KEYS)
+		for i := range keys {
+			keys[i] = rand.Uint64()
+		}
+		filter, error := PopulateBinaryFuse8(keys)
+		assert.Equal(t, nil, error)
+		for _, v := range keys {
+			assert.Equal(t, true, filter.Contains(v))
+		}
+	}
+}
+
+func TestBinaryFuse8Small(t *testing.T) {
+	keys := make([]uint64, SMALL_NUM_KEYS)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+	filter, _ := PopulateBinaryFuse8(keys)
+	for _, v := range keys {
+		assert.Equal(t, true, filter.Contains(v))
+	}
+	falsesize := 10000000
+	matches := 0
+
+	for i := 0; i < falsesize; i++ {
+		v := rand.Uint64()
+		if filter.Contains(v) {
+			matches++
+		}
+	}
+	cut := 1000
+	if cut > SMALL_NUM_KEYS {
+		cut = SMALL_NUM_KEYS
+	}
+	keys = keys[:cut]
+	for trial := 0; trial < 10; trial++ {
+		rand.Seed(int64(trial))
+		for i := range keys {
+			keys[i] = rand.Uint64()
+		}
+		filter, _ = PopulateBinaryFuse8(keys)
+		for _, v := range keys {
+			assert.Equal(t, true, filter.Contains(v))
+		}
+
+	}
+}
+
+
+func BenchmarkBinaryFuse8Populate1000000(b *testing.B) {
+	keys := make([]uint64, NUM_KEYS, NUM_KEYS)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		PopulateBinaryFuse8(keys)
+	}
+}
+
+func Test_ZeroSet(t *testing.T) {
+	keys := []uint64{}
+	_, err := PopulateBinaryFuse8(keys)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+}
+
+func Test_DuplicateKeysBinaryFuseDup(t *testing.T) {
+	keys := []uint64{303, 1, 77, 31, 241, 303}
+	_, err := PopulateBinaryFuse8(keys)
+	if err != nil {
+		t.Fatalf("Unexpected error: %v", err)
+	}
+}
+
+var bogusbinary *BinaryFuse8
+
+
+func BenchmarkConstructBinaryFuse8(b *testing.B) {
+	bigrandomarrayInit()
+	b.ResetTimer()
+	b.ReportAllocs()
+	for n := 0; n < b.N; n++ {
+		PopulateBinaryFuse8(bigrandomarray)
+	}
+}
+
+var bigrandomarray []uint64
+
+const CONSTRUCT_SIZE = 100000000
+
+func bigrandomarrayInit() {
+	if bigrandomarray == nil {
+		fmt.Println("bigrandomarray setup with CONSTRUCT_SIZE = ", CONSTRUCT_SIZE)
+		bigrandomarray = make([]uint64, CONSTRUCT_SIZE)
+		for i := range bigrandomarray {
+			bigrandomarray[i] = rand.Uint64()
+		}
+	}
+}
+
+
+func BenchmarkBinaryFuse8Contains1000000(b *testing.B) {
+	keys := make([]uint64, NUM_KEYS, NUM_KEYS)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+	filter, _ := PopulateBinaryFuse8(keys)
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		filter.Contains(keys[n%len(keys)])
+	}
+}
+
+
+var binaryfusedbig *BinaryFuse8
+
+func binaryfusedbigInit() {
+	fmt.Println("Binary Fuse setup")
+	keys := make([]uint64, 50000000, 50000000)
+	for i := range keys {
+		keys[i] = rand.Uint64()
+	}
+	binaryfusedbig, _ = PopulateBinaryFuse8(keys)
+	fmt.Println("Binary Fuse setup ok")
+}
+
+func BenchmarkBinaryFuse8Contains50000000(b *testing.B) {
+	if binaryfusedbig == nil {
+		binaryfusedbigInit()
+	}
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		binaryfusedbig.Contains(rand.Uint64())
+	}
+}
+
